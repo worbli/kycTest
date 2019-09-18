c4585a8dccde76cf3572ecef4dc10b7279eeef0d
bkawk <email.will.in.china@gmail.com>
2019-09-18 19:15:51 +0800

    docs

---
+++ docs/precache-manifest.66db19de39f7d5183d555f833628c57a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9387a8e337a8d81159c3d57383a37c22",
+    "url": "/kycTest/index.html"
+  },
+  {
+    "revision": "db610e3e73668579aa99",
+    "url": "/kycTest/static/css/main.68c23e3d.chunk.css"
+  },
+  {
+    "revision": "5b565f1298de29ac0043",
+    "url": "/kycTest/static/js/2.e58a6f6a.chunk.js"
+  },
+  {
+    "revision": "db610e3e73668579aa99",
+    "url": "/kycTest/static/js/main.9a75fd4d.chunk.js"
+  },
+  {
+    "revision": "3428a611d098c38a8bdd",
+    "url": "/kycTest/static/js/runtime~main.3d768f92.js"
+  },
+  {
+    "revision": "6d833f1e933ba75e9de622cf76b97a1e",
+    "url": "/kycTest/static/media/chevron-down.6d833f1e.svg"
+  }
+]);
